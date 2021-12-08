ff4d2fc7f63be7949a35aa75d8d0545728188fa8
Juand0145 <juand0145@gmail.com>
2021-12-07 20:32:16 -0500

    [Adding] task 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Method that determines if all the boxes can be opened."""
+
+
+def unlocking_boxes(boxes, index_box, opened_boxes, keys):
+    """Recursive function to open the boxes"""
+
+    n_boxes = len(boxes)
+    expected_boxes = list(range(0, n_boxes))
+
+    # Number of the boxes opened
+    if index_box not in opened_boxes:
+        opened_boxes.append(index_box)
+        opened_boxes.sort()
+
+    # number of keys we belong
+    for key in boxes[index_box]:
+        if key not in keys:
+            keys.append(key)
+
+    # Verify if the keys can open all the boxes
+    if expected_boxes == opened_boxes:
+        return True
+
+    for key in keys:
+        if key not in opened_boxes:
+            return unlocking_boxes(boxes, key, opened_boxes, keys)
+
+    return False
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened."""
+    opened_boxes = [0]
+    keys = []
+
+    result = unlocking_boxes(boxes, 0, opened_boxes, keys)
+    return result
