d0ffce46df36fc60480c0704d7dab3165c07b831
Juan David Tuta Botero <juand0145@gmail.com>
2022-01-04 18:40:26 -0500

    [Adding] task 0

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""File that contains the function minOperations"""
+
+
+def minOperations(n):
+    """
+    function that calculates the minimun operations
+    Args:
+    n: number of character to acomplish
+    return 0 the minimun operation or None
+    """
+    if type(n) is not int or n < 1:
+        return 0
+
+    min_iterations = n
+    divisors = [1]
+
+    for i in range(int(n/2))[2:]:
+        if n % i == 0:
+            second = int(n/i)
+            iterations = second + i
+
+            if iterations < min_iterations:
+                min_iterations = iterations
+
+    return min_iterations
