da87045ead2e51331591a5ff1fe0ebee32f2697a
Juan David Tuta Botero <juand0145@gmail.com>
2022-01-05 15:58:41 -0500

    [Fixing] changing the loop concept

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -12,14 +12,13 @@ def minOperations(n):
     if type(n) is not int or n < 2:
         return 0
 
-    min_iterations = n
+    operations = 0
+    i = 2
 
-    for i in range(int(n/2))[2:]:
-        if n % i == 0:
-            second = int(n/i)
-            iterations = second + i
-
-            if iterations < min_iterations:
-                min_iterations = iterations
-
-    return min_iterations
+    while(i <= n + 1):
+        if (n % i == 0):
+            operations += i
+            n = n/i
+        else:
+            i += 1
+    return operations
