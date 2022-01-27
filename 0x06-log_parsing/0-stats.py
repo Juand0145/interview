a047e01c890222bf3286a3163c8c61c4df5814eb
Juand0145 <juand0145@gmail.com>
2022-01-27 15:31:16 -0500

    [Fixing] making axecuatble

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Reads stdin line by line and computes metrics"""
+"""script that reads stdin line by line and computes metrics"""
 
 if __name__ == "__main__":
     import sys
