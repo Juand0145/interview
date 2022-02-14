8375c7d0703b4d72ba0fae50a639f314316efa03
Juand0145 <juand0145@gmail.com>
2022-02-14 09:07:03 -0500

    [Adding] creating README file and adding task 0

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""File that contains the function validUTF8"""
+
+
+def validUTF8(data):
+    """
+    Function that  determines if a given data set represents
+    a valid UTF-8 encoding
+    Args:
+    data is a list of numbers to analize
+    Return: True if data is a valid UTF-8 encoding, else return False
+    """
+    byte_count = 0
+    first = 1 << 7
+    second = 1 << 6
+
+    for n in data:
+        condition = 1 << 7
+        if byte_count == 0:
+            while condition & n:
+                byte_count += 1
+                condition = condition >> 1
+            if byte_count == 0:
+                continue
+            if byte_count == 1 or byte_count > 4:
+                return False
+        else:
+            if not (n & first and not (n & second)):
+                return False
+        byte_count -= 1
+    return byte_count == 0
