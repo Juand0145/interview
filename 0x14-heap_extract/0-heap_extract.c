5ffb6b7bb3c3e9a5c06de565b45d82389690ee35
Juand0145 <juand0145@gmail.com>
2022-05-11 08:55:45 -0500

    [Fixing] the entire prototype file

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -1,74 +1,173 @@
 #include "binary_trees.h"
 
 /**
- * heap_sort - sorts an array of ints in ascending order
- * using Heap Sort algorithm
- * @array: the array of ints to sort
- * @size: the size of the array to sort
+ * heap_extract - extracts the root node of a Max Binary Heap
+ * function returns the value extracted, replaces with last level-order node,
+ * and rebuilds Max Binary Heap, if needed
+ *
+ * @root: double pointer to the root node of the heap
+ *
+ * Return: value stored in extracted node, or 0 if failed
  */
-void heap_sort(int *array, size_t size)
+
+int heap_extract(heap_t **root)
 {
-if (array == NULL || size <= 1)
+int value = 0, check = 1;
+size_t height = 0, level = 0;
+heap_t *left = NULL, *right = NULL, *last = NULL;
+
+/* NULL checks */
+if (root == NULL || *root == NULL)
+return (0);
+
+/* if not NULL, initialize variables with root node info */
+value = (*root)->n;
+left = (*root)->left;
+right = (*root)->right;
+
+/* find replacement node (last level-order node) */
+height = binary_tree_height(*root);
+for (level = 0; level <= height; level++)
+find_replacement(*root, level, &last);
+
+/* extract root and replace with found last level order node */
+free_and_replace(root, &left, &right, &last);
+
+/* heapify (swap) to maintain Max Binary Heap */
+while (last && check)
+heapify(root, last, &check);
+
+return (value);
+}
+
+/**
+ * binary_tree_height - finds the height of completed binary tree
+ * since the tree is completed, tallest on left side
+ * @root: pointer to the root node of the tree
+ *
+ * Return: returns the height of the overall tree
+ */
+
+size_t binary_tree_height(heap_t *root)
+{
+if (root == NULL)
+return (0);
+return (1 + binary_tree_height(root->left));
+}
+
+/**
+ * find_replacement - finds first location that does not have a right child
+ * the location may be a leaf or only have left child
+ * since level-order traversal, finds location of last node for complete tree
+ * @root: pointer to the root node of the tree
+ * @level: keeps track of the current level in the tree
+ * @last: double pointer to current last node,
+ * updates while moving through level order
+ *
+ * Return: location of last node in level order, which will replace root node
+ */
+
+void find_replacement(heap_t *root, size_t level, heap_t **last)
+{
+if (root == NULL)
 return;
-heap_sort_print(array, size, array, size);
+if (level == 0)
+(*last) = root;
+find_replacement(root->left, level - 1, last);
+find_replacement(root->right, level - 1, last);
 }
 
 /**
- * heap_sort_print - heap sort with full array as parameter for printing
- * @array: array to sort
- * @size: size of array to sort
- * @array_p: array to print
- * @size_p: size of array to print
+ * free_and_replace - extracts and frees the root node, then
+ * replaces the root of the heap with last level order node
+ *
+ * @root: double pointer to root node to extract
+ * @left: double pointer to root node's left child or NULL
+ * @right: double pointer to root node's right child or NULL
+ * @last: double pointer to last node from level order traversal
  */
-void heap_sort_print(int *array, size_t size, int *array_p, size_t size_p)
-{
-int i = 0, temp = 0;
 
-for (i = (size - 1) / 2; i >= 0; i--)
+void free_and_replace(heap_t **root, heap_t **left,
+heap_t **right, heap_t **last)
 {
-heapify(array, size, i, array_p, size_p);
+/* extract and reset root pointer if only node */
+if (*last == *root)
+{
+free(*root);
+*root = NULL;
+*last = NULL;
+return;
 }
-for (i = (size - 1); i > 0; i--)
-{
-temp = array[i];
-array[i] = array[0];
-array[0] = temp;
-array_p[i] = array[i];
-array_p[0] = array[0];
-print_array(array_p, size_p);
+/* extracts root node and replaces with node found above */
+if ((*last)->parent->left == (*last))
+(*last)->parent->left = NULL;
+else if ((*last)->parent->right == (*last))
+(*last)->parent->right = NULL;
+(*last)->parent = NULL;
+free(*root);
+*root = *last;
 
-heapify(array, i, 0, array_p, size_p);
+if ((*left) != (*last))
+{
+(*last)->left = (*left);
+if (*left)
+(*left)->parent = (*last);
+}
+if ((*right) != (*last))
+{
+(*last)->right = (*right);
+if (*right)
+(*right)->parent = (*last);
 }
 }
 
 /**
- * heapify - reorders array like nodes swapping in a heap
- * @array: the array of ints to sort
- * @size: the size of the array to sort
- * @i: current index in array
- * @array_p: array to print
- * @size_p: size of array to print
+ * heapify - swaps nodes to maintain Max Binary Heap
+ * @root: double pointer to root node of heap
+ * @current: pointer to current node
+ * @check: int pointer to flag if need to continue swapping
  */
-void heapify(int *array, size_t size, int i, int *array_p, size_t size_p)
-{
-int max = i;
-int left_child = 2 * i + 1;
-int right_child = 2 * i + 2;
-int temp = 0;
 
-if (left_child < (int)size && array[left_child] > array[max])
-max = left_child;
-if (right_child < (int)size && array[right_child] > array[max])
-max = right_child;
-if (max != i)
+void heapify(heap_t **root, heap_t *current, int *check)
 {
-temp = array[i];
-array[i] = array[max];
-array[max] = temp;
-array_p[i] = array[i];
-array_p[max] = array[max];
-print_array(array_p, size_p);
+heap_t *max = NULL, *left = current->left, *right = current->right;
 
-heapify(array, size, max, array_p, size_p);
+max = current;
+if (left && left->n > max->n)
+max = left;
+if (right && right->n > max->n)
+max = right;
+if (current == max)
+{
+*check = 0;
+return;
 }
+max->parent = current->parent;
+if (current->parent == NULL)
+*root = max;
+else if (current->parent->left == current)
+current->parent->left = max;
+else if (current->parent->right == current)
+current->parent->right = max;
+current->left = max->left;
+if (max->left)
+max->left->parent = current;
+current->right = max->right;
+if (max->right)
+max->right->parent = current;
+if (max == left)
+{
+max->right = right;
+if (right)
+right->parent = max;
+max->left = current;
+}
+else if (max == right)
+{
+max->left = left;
+if (left)
+left->parent = max;
+max->right = current;
+}
+current->parent = max;
 }
--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -1,12 +1,32 @@
-#ifndef BINARY_TREES
-#define BINARY_TREES
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_array(const int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void heap_sort_print(int *array, size_t size, int *array_p, size_t size_p);
-void heapify(int *array, size_t size, int i, int *array_p, size_t size_p);
+/* STRUCTS AND DEFINITIONS */
+/**
+ * struct binary_tree_s - Binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
+} binary_tree_t;
 
+typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
+int heap_extract(heap_t **root);
+size_t binary_tree_height(heap_t *root);
+void find_replacement(heap_t *root, size_t level, heap_t **last);
+void free_and_replace(heap_t **root, heap_t **left,
+heap_t **right, heap_t **last);
+void heapify(heap_t **root, heap_t *current, int *check);
 #endif
